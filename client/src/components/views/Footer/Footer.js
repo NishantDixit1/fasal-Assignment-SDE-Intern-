f5b16ac3d06118be085b8fb1e2d985e237393376
NishantDixit1 <nishantpandit2004@gmail.com>
2024-05-31 11:58:30 +0530

    first commit

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Icon} from 'antd';
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p> FASAL Grow more, grow better⚡❤️‍🔥  <Icon type="smile" /></p>
+        </div>
+    )
+}
+
+export default Footer
